82f1f5da42601caec5a0a5fc114ff0aa2dd45495
elvio <elvdp@hotmail.com>
2022-11-21 18:07:19 -0300

    221122
    221122

--- src/app/service/image.service.ts
+++ src/app/service/image.service.ts
@@ -12,20 +12,23 @@ export class ImageService {
     const file = $event.target.files[0]
     const imgRef = ref(this.storage, `imagen/` + name)
     uploadBytes(imgRef, file)
+
     .then(response => {this.getImages()})
     .catch(error => console.log(error))
 
   }
   getImages(){
-    const imagesRef = ref(this.storage, `imagen`)
+    const imagesRef = ref(this.storage, 'imagen');
+
     list(imagesRef)
+
     .then(async response => {
       for(let item of response.items){
         this.url = await getDownloadURL(item);
         console.log("la url es" + this.url);
       }
     })
-  .catch(error => console.log(error))
+  .catch(error => console.log(error));
 
   }
 
