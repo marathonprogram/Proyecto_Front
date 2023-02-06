97f6d8a6378f88e5db2e8fc991a44dfb2f7dbcb2
marathonprogram <108866723+marathonprogram@users.noreply.github.com>
2023-02-06 04:51:16 -0300

    Update s-experiencia.service.ts

--- src/app/service/s-experiencia.service.ts
+++ src/app/service/s-experiencia.service.ts
@@ -8,7 +8,7 @@ import { Experiencia } from '../model/experiencia';
   providedIn: 'root'
 })
 export class SExperienciaService {
-  expURL = 'https://bkdprueba2803.herokuapp.com/explab/';
+  expURL = 'https://back18.onrender.com/explab/';
 
   constructor(private httpClient: HttpClient) {}
 
